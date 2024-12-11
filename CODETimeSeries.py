6849261c5ffca2254f29d624c80fb0a3b30fae67
mintylees <32243076+mintylees@users.noreply.github.com>
2024-12-11 18:50:25 +0000

    Update CODETimeSeries.py

--- CODETimeSeries.py
+++ CODETimeSeries.py
@@ -38,5 +38,45 @@ df['Lower'] = df['Close: 20 Day Mean'] - 2*(df['Close'].rolling(20).std())
 # Plot Close
 df[['Close','Close: 20 Day Mean','Upper','Lower']].plot(figsize=(16,6))
 
+import statsmodels.api as sm
+# import dataset with load_pandas method and .data attribute
+df = sm.datasets.macrodata.load_pandas().data
+df.head()
+index = pd.Index(sm.tsa.datetools.dates_from_range('1959Q1','2009Q3'))
+df.index = index
+df['realgdp'].plot()
+
+sm.tsa.filters.hpfilter(df['realgdp'])
+# let's use tuple unpacking to get this trend and plot it on top of this
+gdp_cycle, gdp_trend = sm.tsa.filters.hpfilter(df['realgdp'])
+
+# add a a column for the trend
+df['trend'] = gdp_trend
+
+# plot the real gdp & the trend
+df[['realgdp','trend']].plot()
+
+from statsmodels.tsa.seasonal import seasonal_decompose
+result = seasonal_decompose(df['Adj Close'], model='additive', freq=12)
+result.trend.plot()
+
+result.trend.plot()
+
+# create 1 month SMA off of Adj Close
+df['30 Day SMA'] = df['Adj Close'].rolling(window=30).mean()
+
+# plot SMA & Adj Close
+df[['Adj Close', '30 Day SMA']].plot(figsize=(10,8))
+
+# create EWMA
+df['EWMA-30'] = df['Adj Close'].ewm(span=30).mean()
+
+# plot EWMA
+df[['Adj Close', 'EWMA-30']].plot(figsize=(10,8))
+
+
+
+
+
 
 
